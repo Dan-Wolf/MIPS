f95722a509215b6b2a002f6c55d4d62583692f7e
Dan-Wolf <56487557+Dan-Wolf@users.noreply.github.com>
2021-11-18 14:06:36 -0500

    instruction.sv

--- instructions.sv
+++ instructions.sv
@@ -2,15 +2,22 @@
 Filename:       instructions.sv
 Author:         DTW
 Date:           11/17/2021
-Description:    This module contains the instuction memory. Will be programmed with Machine language.
+Description:    This module contains the instuction memory, it reads in a hex file called "instructions.mem".   
+                Will be programmed with Machine language.
 */
 
 //=================================================================================================
 
 module instruction_mem #(
     MEM_SIZE = 256
-) ( input
-    
+) ( input   logic   [31:0]  PC,
+    output  logic   [31:0]  instuction
 );
+
+    logic [31:0] instruction_mem [0:MEM_SIZE-1];
+    $readmemh("instructions.mem", instruction_mem);
+
+    assign instruction = instruction_mem[PC];
+
     
 endmodule
